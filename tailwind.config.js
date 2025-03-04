946b4c4cb6744f03f8ac8386c386869f494f229b
Arush <arushmaurya736@gmail.com>
2025-03-04 22:23:05 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          a: ['"Style Script"', 'cursive'], // Double Quotes Lagana Zaroori Hai
+        },
+      },
+    },
+    plugins: [],
+  }
+  
